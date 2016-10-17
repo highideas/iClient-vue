938cb684066d6fb23bae1f48c68bcf2a795ee1d3
gabriel.goncalves <gabriel.goncalves@fidelize.com.br>
2016-10-17 18:00:32 -0200

    * Create component of area to list visits by areas

--- src/components/Areas.vue
+++ src/components/Areas.vue
@@ -3,29 +3,23 @@
 
   <div class="container hello">
     <div class="area" v-for="area in areas">
-        <h3 class="title is-3">{{ area.name }}</h3>
+        <h3 class="title is-3">{{area._id}}</h3>
         <table class="table">
             <thead>
                 <tr>
                     <th>Name</th>
                     <th>Last Visit</th>
                     <th></th>
-                    <th></th>
                 </tr>
             </thead>
             <tbody>
-                <tr v-for="client in area.clients">
-                    <td>{{ client.name }}</td>
-                    <td>{{ client.visita }}</td>
+                <tr v-for="areaVisit in area.visits">
+                    <td>{{ areaVisit.visit.client.name }}</td>
+                    <td>{{ areaVisit.visit.visit_date }}</td>
                     <td class="is-icon">
-                        <a href="#">
-                            <i class="fa fa-calendar"></i>
-                        </a>
-                    </td>
-                    <td class="is-icon">
-                        <a href="#">
-                            <i class="fa fa-search"></i>
-                        </a>
+                        <router-link :to="{ name: 'visit', params: { id: areaVisit.visit._id }}">
+                            <i class="fa fa-info-circle"></i>
+                        </router-link>
                     </td>
                 </tr>
             </tbody>
@@ -42,41 +36,20 @@
 export default {
   data () {
     return {
-      msg: 'Clients',
-    clients: {},
-        areas : [
-            { 
-                name: "Area 1",
-                clients: [
-                    {name: "Client 1", visita: "10/09/2006"},
-                    {name: "Client 2", visita: "11/09/2006"},
-                    {name: "Client 3", visita: "10/09/2006"}
-                ]
-            },
-            { 
-                name: "Area 2",
-                clients: [
-                    {name: "Client 1", visita: "11/09/2006"},
-                    {name: "Client 2", visita: "10/09/2006"},
-                ]
-            }
-        ]
+        areas: []
     }
   },
     created: function (){
         console.log(localStorage.getItem('jwt-token'));
-            this.$http.get('http://localhost:3000/api/v1/client', {
+            this.$http.get('http://localhost:3000/api/v1/visit/group/area', {
                  headers: {
                     Authorization : localStorage.token
                 }
             }).then(function (res) {
-                console.log(res);
-                this.clients = res.data.clients;
+                this.areas = res.data.visits;
             }, function (err) {
                 console.log(err);
             });
     },
-    methods: {
-    }
 }
 </script>
