5ce56f43f4abefd45150ed87711cebdb10ecf27a
gabriel.goncalves <gabriel.goncalves@fidelize.com.br>
2016-10-24 18:25:40 -0200

    * Change from browsefiry to webpack
     * Adding route to show details of visit

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 import VueResource from 'vue-resource'
 import VueRouter from 'vue-router'
+import 'bulma/css/bulma.css'
+import 'font-awesome/css/font-awesome.min.css'
 
 Vue.use(VueResource)
 Vue.use(VueRouter)
